f4ace21973fd5625e0b4108c425345d6ae204190
Shin Kawano <orenomail@gmail.com>
2014-12-19 14:56:21 +0900

    add saving file and resume

--- PRIDE2RDF.rb
+++ PRIDE2RDF.rb
@@ -3,7 +3,11 @@ require 'uri'
 require 'json'
 require 'rdf'
 require 'rdf/turtle'
+require 'uuid'
 include RDF
+
+output = "/Volumes/orenostorage/PRIDE/RDF"
+rdf    = ""
 
 def get_jsons(uri)
   return get_json(uri)["list"]
@@ -25,34 +29,34 @@ def literal_p(word)
   return "\"#{word}\""
 end
 
-def generate_rdf(uri, i)
+def generate_rdf(uri, i, f)
   i.each do |key, value|
-    if value.kind_of?(Float) || value && value.size > 0
+    if value.kind_of?(Float) || value && value.to_s.size > 0
       if value.kind_of?(Array)
         value.each do |v|
           if v.kind_of?(Hash)
             bnode = RDF::Node.uuid.to_s.delete("-")
-            puts "#{uri_p(uri)} <http://www.ebi.ac.uk/pride/rdf/#{key}> #{bnode} ."
-            puts "#{bnode} <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.ebi.ac.uk/pride/rdf/#{key.sub(/s$/, "").capitalize}> ."
+            f.puts "#{uri_p(uri)} <http://www.ebi.ac.uk/pride/rdf/#{key}> #{bnode} ."
+            f.puts "#{bnode} <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.ebi.ac.uk/pride/rdf/#{key.sub(/s$/, "").capitalize}> ."
     
             v.each do |k, v2|
-              puts "#{bnode} <http://www.ebi.ac.uk/pride/rdf/#{k}> #{literal_p(v2)} ."
+              f.puts "#{bnode} <http://www.ebi.ac.uk/pride/rdf/#{k}> #{literal_p(v2)} ."
             end
         
           else
-            puts "#{uri_p(uri)} <http://www.ebi.ac.uk/pride/rdf/#{key}> #{literal_p(v)} ."
+            f.puts "#{uri_p(uri)} <http://www.ebi.ac.uk/pride/rdf/#{key}> #{literal_p(v)} ."
           end
         end
       elsif value.kind_of?(Hash)
         bnode = RDF::Node.uuid.to_s.delete("-")
-        puts "#{uri_p(uri)} <http://www.ebi.ac.uk/pride/rdf/#{key}> #{bnode} ."
-        puts "#{bnode} <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.ebi.ac.uk/pride/rdf/#{key.sub(/s$/, "").capitalize}> ."
+        f.puts "#{uri_p(uri)} <http://www.ebi.ac.uk/pride/rdf/#{key}> #{bnode} ."
+        f.puts "#{bnode} <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.ebi.ac.uk/pride/rdf/#{key.sub(/s$/, "").capitalize}> ."
     
         value.each do |k, v|
-          puts "#{bnode} <http://www.ebi.ac.uk/pride/rdf/#{k}> #{literal_p(v)} ."
+          f.puts "#{bnode} <http://www.ebi.ac.uk/pride/rdf/#{k}> #{literal_p(v)} ."
         end
       else
-        puts "#{uri_p(uri)} <http://www.ebi.ac.uk/pride/rdf/#{key}> #{literal_p(value)} ."
+        f.puts "#{uri_p(uri)} <http://www.ebi.ac.uk/pride/rdf/#{key}> #{literal_p(value)} ."
       end
     end
   end
@@ -79,61 +83,136 @@ rdf   = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
 
 
 #########################################################
-#  retrieving project metadata
+#  project accession numbers in PRIDE
 #########################################################
-pid = "PXD001328"
-
-project     = get_json("http://wwwdev.ebi.ac.uk/pride/ws/archive/project/#{pid}")
-#project     = JSON.parse(File.open("sample_data/project.json").read)
-project_uri = base_uri + pid
-puts "#{uri_p(project_uri)} <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.ebi.ac.uk/pride/rdf/PrideProject> ."
-
-generate_rdf(project_uri, project)
+project_number = get_uri("http://wwwdev.ebi.ac.uk/pride/ws/archive/project/count")
+projects       = get_jsons("http://wwwdev.ebi.ac.uk/pride/ws/archive/project/list?show=#{project_number}")
 
 
 #########################################################
-#  retrieving project assays
+#  resume
 #########################################################
-num_assays = project["numAssays"]
-assays = get_jsons("http://wwwdev.ebi.ac.uk/pride/ws/archive/assay/list/project/#{pid}?show=#{num_assays}")
-#assays = JSON.parse(File.open("sample_data/assays.json").read)["list"]
+=begin
+accessions = Array.new
+projects.each do |k|
+  accessions << k["accession"]
+end
+skip = accessions.index("PRD000066")
+projects.shift(skip)
+=end
 
-assays.each do |assay|
-  assay_uri = base_uri + pid + "/assays/" + assay["assayAccession"]
-  puts "#{uri_p(project_uri)} #{uri_p(pride + "has_assay")} #{uri_p(assay_uri)} ."
-  puts "#{uri_p(assay_uri)} #{uri_p(rdf + "type")} <http://www.ebi.ac.uk/pride/rdf/Assay> ."
 
-  generate_rdf(assay_uri, assay)
-
-  
+projects.each do |project|
   #########################################################
-  #  retrieving proteins in assay
+  #  retrieving project metadata
   #########################################################
-  proteins = get_jsons("http://wwwdev.ebi.ac.uk/pride/ws/archive/protein/list/assay/#{assay["assayAccession"]}?show=#{assay["proteinCount"]}")
-  #proteins = JSON.parse(File.open("sample_data/proteins.json").read)["list"]
-  proteins.each do |protein|
-    protein_uri = [assay_uri, protein["accession"]].join("/")
-    puts "#{uri_p(assay_uri)} <http://www.ebi.ac.uk/pride/rdf/has_protein> #{uri_p(protein_uri)} ."
-    puts "#{uri_p(protein_uri)} <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.ebi.ac.uk/pride/rdf/Protein> ."
+  #pid = "PXD000605"
+  pid = project["accession"]
+
+  unless pid == "PXT000186" || pid == "PXT000177" || pid == 'PRD000721' || pid == 'PRD000066'
+    f = File.open("#{output}/#{pid}.nt", "w")
+
+    project_json = get_json("http://wwwdev.ebi.ac.uk/pride/ws/archive/project/#{pid}")
+    #project_json = JSON.parse(File.open("sample_data/project.json").read)
+    project_uri  = base_uri + pid
+
+    puts project_uri
+
+    f.puts "#{uri_p(project_uri)} <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.ebi.ac.uk/pride/rdf/PrideProject> ."
+
+    generate_rdf(project_uri, project_json, f)
+
+
+    #########################################################
+    #  retrieving file information
+    #########################################################
+    #files = get_jsons("http://wwwdev.ebi.ac.uk:80/pride/ws/archive/file/list/project/#{pid}")
+    #files.each do |file|
+    #  file[downloadLink]
+    #end
+
+    #########################################################
+    #  retrieving project assays
+    #########################################################
+    num_assays = project["numAssays"]
+    puts num_assays
     
-    generate_rdf(protein_uri, protein)
-    #break
-  end
-  
-  
-  #########################################################
-  #  retrieving peptides in assay
-  #########################################################
-  peptides = get_jsons("http://wwwdev.ebi.ac.uk/pride/ws/archive/peptide/list/assay/#{assay["assayAccession"]}?show=#{assay["peptideCount"]}")
-  #peptides = JSON.parse(File.open("sample_data/peptides.json").read)["list"]
-  peptides.each do |peptide|
-    peptide_uri = "http://www.ebi.ac.uk/pride/archive/projects/PXD001328/assays/39777/#{peptide["id"]}"
-    puts "#{uri_p(assay_uri)} <http://www.ebi.ac.uk/pride/rdf/detected_peptide> #{uri_p(peptide_uri)} ."
-    puts "#{uri_p(peptide_uri)} <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.ebi.ac.uk/pride/rdf/Peptide> ."
-    
-    generate_rdf(peptide_uri, peptide)
-    #break
-  end
+    assays = get_jsons("http://wwwdev.ebi.ac.uk/pride/ws/archive/assay/list/project/#{pid}?show=#{num_assays}")
+    #assays = JSON.parse(File.open("sample_data/assays.json").read)["list"]
 
+    assays.each do |assay|
+      assay_uri = base_uri + pid + "/assays/" + assay["assayAccession"]
+  
+      puts assay_uri
+  
+      f.puts "#{uri_p(project_uri)} #{uri_p(pride + "has_assay")} #{uri_p(assay_uri)} ."
+      f.puts "#{uri_p(assay_uri)} #{uri_p(rdf + "type")} <http://www.ebi.ac.uk/pride/rdf/Assay> ."
+
+      generate_rdf(assay_uri, assay, f)
+
+  
+      #########################################################
+      #  retrieving proteins in assay
+      #########################################################
+      puts assay["proteinCount"]
+      if assay["proteinCount"] == 0
+        assay["proteinCount"] = 1
+      end
+      proteins = get_jsons("http://wwwdev.ebi.ac.uk/pride/ws/archive/protein/list/assay/#{assay["assayAccession"]}?show=#{assay["proteinCount"]}")
+      #proteins = JSON.parse(File.open("sample_data/proteins.json").read)["list"]
+      
+      puts proteins.size
+      proteins.each do |protein|
+        protein_uri = [assay_uri, protein["accession"]].join("/")
+    
+        #puts protein_uri
+    
+        f.puts "#{uri_p(assay_uri)} <http://www.ebi.ac.uk/pride/rdf/has_protein> #{uri_p(protein_uri)} ."
+        f.puts "#{uri_p(protein_uri)} <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.ebi.ac.uk/pride/rdf/Protein> ."
+    
+        generate_rdf(protein_uri, protein, f)
+        #break
+      end
+
+  
+      #########################################################
+      #  retrieving peptides in assay
+      #########################################################
+      peptides = Array.new
+      peptide_count = 0
+      page = 0
+      
+      puts assay["peptideCount"]
+      if assay["peptideCount"] == 0
+        assay["peptideCount"] = 1
+      end
+      if assay["peptideCount"].to_i > 50000
+        while assay["peptideCount"].to_i > peptide_count
+          peptides += get_jsons("http://wwwdev.ebi.ac.uk/pride/ws/archive/peptide/list/assay/#{assay["assayAccession"]}?show=50000&page=#{page}")
+          peptide_count += 50000
+          page += 1
+          puts peptide_count
+        end
+      else
+        peptides = get_jsons("http://wwwdev.ebi.ac.uk/pride/ws/archive/peptide/list/assay/#{assay["assayAccession"]}?show=#{assay["peptideCount"]}")
+        #peptides = JSON.parse(File.open("sample_data/peptides.json").read)["list"]
+      end
+      
+      puts peptides.size
+      peptides.each do |peptide|
+        peptide_uri = [assay_uri, peptide["id"]].join("/")
+    
+        #puts peptide_uri
+    
+        f.puts "#{uri_p(assay_uri)} <http://www.ebi.ac.uk/pride/rdf/detected_peptide> #{uri_p(peptide_uri)} ."
+        f.puts "#{uri_p(peptide_uri)} <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.ebi.ac.uk/pride/rdf/Peptide> ."
+    
+        generate_rdf(peptide_uri, peptide, f)
+        #break
+      end
+      #break
+    end
+    f.close
+  end
   #break
 end
